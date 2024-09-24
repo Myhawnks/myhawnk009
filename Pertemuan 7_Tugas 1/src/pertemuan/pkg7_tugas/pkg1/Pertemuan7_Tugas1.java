e101f00a9486aa091900b17da6b007175b02ebda
Jibrils <145530150+Myhawnks@users.noreply.github.com>
2024-09-24 10:02:38 +0700

    Add files via upload

---
+++ Pertemuan 7_Tugas 1/src/pertemuan/pkg7_tugas/pkg1/Pertemuan7_Tugas1.java
@@ -0,0 +1,18 @@
+package pertemuan.pkg7_tugas.pkg1;
+
+public class Pertemuan7_Tugas1 {
+    public static void main(String[] args) {
+        int day = 2;
+        switch(day){
+                case 1:
+                System.out.println("Sabtu");
+                break;
+                case 2:
+                System.out.println("Minggu");
+                break;
+                default:
+                    System.out.println("Sistem Opersi, Pemograman Java, Struktur Data");
+                    break;
+        }
+    }
+}

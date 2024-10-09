39649b81cc8a133c91c1da6bbbe12b63ec65956c
Jibrils <145530150+Myhawnks@users.noreply.github.com>
2024-10-09 09:13:00 +0700

    Add files via upload

---
+++ Pertemuan 9_Tugas 3/src/pertemuan/pkg9_tugas/pkg3/Pertemuan9_Tugas3.java
@@ -0,0 +1,11 @@
+package pertemuan.pkg9_tugas.pkg3;
+
+public class Pertemuan9_Tugas3 {
+    public static void main(String[] args) {
+        int i = 1;
+         while (i < 6) {
+         System.out.println(i);
+         i++; // Menambahkan 1 ke i
+        }
+    }
+}
